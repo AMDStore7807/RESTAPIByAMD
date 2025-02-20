fe5bb8a9fec7e1a9f434433b034f185b1591c0cb
Abdul Malik Karim Amarullah <malikamka122@gmail.com>
2025-02-21 01:10:34 +0700

    ganti dashboard.html menjadi index.html

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.get('/api/orkut/cekstatus', async (req, res) => {
 
 
 app.get('/', (req, res) => {
-res.sendFile(path.join(__dirname, 'dashboard.html'));
+res.sendFile(path.join(__dirname, 'index.html'));
 })
 
 
