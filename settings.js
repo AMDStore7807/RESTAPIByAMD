bda4462caaa914f81784871f06883e11b5303416
AMDStore7807 <malikamka122@gmail.com>
2025-02-24 01:13:20 +0700

    add search

--- settings.js
+++ settings.js
@@ -1,2 +1,11 @@
+const expiryDurations = {};
+for (let i = 1; i <= 30; i++) {
+  expiryDurations[`${i}day`] = i * 24 * 60 * 60 * 1000;
+}
+global.expiryDurations = expiryDurations;
 global.creator = "AMD STORE";
-global.apikey = ["amdsukarulzz"];
+global.apikey = [
+  { key: "amdsukarulzz", expired: "permanent", createdAt: new Date() },
+  { key: "tempkey123", expired: "1day", createdAt: new Date() },
+  { key: "tempkey1237", expired: "21day", createdAt: new Date() },
+];
