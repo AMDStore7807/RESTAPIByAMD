ad54c00c5341e2d54490a7b9afbd22d7b4538d6a
AMDStore7807 <malikamka122@gmail.com>
2025-02-28 01:47:54 +0700

    Revert "add search yes"
    This reverts commit 2e5224ccb42fbc20db48f626a6d8bf9341056369.

--- settings.js
+++ settings.js
@@ -1,11 +1,2 @@
-const expiryDurations = {};
-for (let i = 1; i <= 30; i++) {
-  expiryDurations[`${i}day`] = i * 24 * 60 * 60 * 1000;
-}
-global.expiryDurations = expiryDurations;
 global.creator = "AMD STORE";
-global.apikey = [
-  { key: "amdsukarulzz", expired: "permanent", createdAt: new Date() },
-  { key: "tempkey123", expired: "1day", createdAt: new Date() },
-  { key: "tempkey1237", expired: "21day", createdAt: new Date() },
-];
+global.apikey = ["amdsukarulzz"];
