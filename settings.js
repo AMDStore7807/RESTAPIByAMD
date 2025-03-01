4e0468035b71b6f3b5e72860f44c191ee0e16e43
AMDStore7807 <malikamka122@gmail.com>
2025-03-01 17:32:41 +0700

    add all fitur

--- settings.js
+++ settings.js
@@ -4,8 +4,3 @@ for (let i = 1; i <= 30; i++) {
 }
 global.expiryDurations = expiryDurations;
 global.creator = "AMD STORE";
-global.apikey = [
-  { key: "amdsukarulzz", expired: "permanent", createdAt: new Date() },
-  { key: "tempkey123", expired: "1day", createdAt: new Date() },
-  { key: "tempkey1237", expired: "21day", createdAt: new Date() },
-];
